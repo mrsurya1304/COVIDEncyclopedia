fe2a79ade42c243c6d78a89295b4b0c1e09b50da
dhilipkarthik01 <61049937+dhilipkarthik01@users.noreply.github.com>
2022-04-26 09:19:12 +0530

    Date Picker Added For CovidForecast

--- app/src/main/java/com/example/androidproject/COVID19_Forecast.java
+++ app/src/main/java/com/example/androidproject/COVID19_Forecast.java
@@ -3,6 +3,7 @@ package com.example.androidproject;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -11,10 +12,9 @@ import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-import android.widget.Spinner;
-import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -26,12 +26,14 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.util.Arrays;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Locale;
 
 public class COVID19_Forecast extends AppCompatActivity {
     public Button predict;
-    EditText place, date;
+    final Calendar myCalendar= Calendar.getInstance();
+    EditText place, date_et;
     ProgressBar progressBar;
     AlertDialog.Builder dialog;
     TextToSpeech sound;
@@ -46,7 +48,23 @@ public class COVID19_Forecast extends AppCompatActivity {
             }
         });
         place = findViewById(R.id.place);
-        date = findViewById(R.id.date);
+        date_et =(EditText) findViewById(R.id.date);
+        DatePickerDialog.OnDateSetListener date =new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH,month);
+                myCalendar.set(Calendar.DAY_OF_MONTH,day);
+                updateLabel();
+            }
+
+        };
+        date_et.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(COVID19_Forecast.this,date,myCalendar.get(Calendar.YEAR),myCalendar.get(Calendar.MONTH),myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
         predict = findViewById(R.id.predict);
 //        Result = findViewById(R.id.result);
         progressBar = findViewById(R.id.progressBar);
@@ -55,7 +73,7 @@ public class COVID19_Forecast extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String i1 = place.getText().toString();
-                String i2 = date.getText().toString();
+                String i2 = date_et.getText().toString();
                 progressBar.setVisibility(View.VISIBLE);
                 String url = "https://covidencyclopedia1.herokuapp.com/?a=" + i1 + "&b=" + i2;
                 RequestQueue queue = Volley.newRequestQueue(COVID19_Forecast.this);
@@ -96,6 +114,15 @@ public class COVID19_Forecast extends AppCompatActivity {
                                     alert.show();
                                 } catch (JSONException jsonException) {
                                     jsonException.printStackTrace();
+                                    dialog.setMessage("Invalid Date!")
+                                            .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                                                @Override
+                                                public void onClick(DialogInterface dialogInterface, int i) {
+                                                }
+                                            });
+                                    AlertDialog alert = dialog.create();
+                                    alert.setTitle("RISK PREDICTION RESULT");
+                                    alert.show();
 //                                    Result.setText("Error! please check!!!");
                                 }
                             }
@@ -112,6 +139,11 @@ public class COVID19_Forecast extends AppCompatActivity {
 
             }
         });
+    }
+    private void updateLabel(){
+        String myFormat="yyyy-MM-dd";
+        SimpleDateFormat dateFormat=new SimpleDateFormat(myFormat, Locale.US);
+        date_et.setText(dateFormat.format(myCalendar.getTime()));
     }
 }
 //import androidx.appcompat.app.AppCompatActivity;
